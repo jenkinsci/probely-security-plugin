3326f9f6fcbd6f3a036ed971cd84f28e79761e72
Joao Poupino <joao@probely.com>
2019-02-21 14:41:25 +0000

    Update tests

--- src/test/java/com/probely/plugin/ProbelyScanBuilderTest.java
+++ src/test/java/com/probely/plugin/ProbelyScanBuilderTest.java
@@ -1,7 +1,11 @@
 package com.probely.plugin;
 
+import com.probely.api.ScanRequest;
+import com.probely.util.ApiUtils;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -15,22 +19,25 @@ public class ProbelyScanBuilderTest {
     public JenkinsRule jenkins = new JenkinsRule();
 
     @Test
-    public void testConfigRoundtrip() throws Exception {
+    public void testBuildEmptyCredentials() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        project.getBuildersList().add(new ProbelyScanBuilder(targetId, authToken));
-        project = jenkins.configRoundtrip(project);
-        jenkins.assertEqualDataBoundBeans(
-                new ProbelyScanBuilder(targetId, authToken),
-                project.getBuildersList().get(0));
+        ProbelyScanBuilder builder = new ProbelyScanBuilder(targetId, authToken);
+
+        project.getBuildersList().add(builder);
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        jenkins.assertLogContains("Credentials not found", build);
     }
 
     @Test
-    public void testBuild() throws Exception {
-        FreeStyleProject project = jenkins.createFreeStyleProject();
-        ProbelyScanBuilder builder = new ProbelyScanBuilder(targetId, authToken);
-        project.getBuildersList().add(builder);
-
-        FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
-        jenkins.assertLogContains("Started scan on target", build);
+    public void testInvalidCredentials() throws Exception {
+        CloseableHttpClient client = ApiUtils.buildHttpClient(5000);
+        try {
+            ScanRequest sr = new ScanRequest(authToken, Settings.API_PROFILE_URL, targetId, client);
+            sr.start();
+        } catch (ClientProtocolException cex) {
+            // Expected
+        } finally {
+            ApiUtils.closeHttpClient(client);
+        }
     }
 }
